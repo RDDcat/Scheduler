2c8542ab33ed1e7a29c634252ddb41889967aebb
RDDcat <yukmaro@gmail.com>
2022-03-12 11:30:43 +0900

    [ADD] 일기목록 캘린더형, 일기추가 액티비티 생성

---
+++ Scheduler/app/src/main/java/com/example/scheduler/AddDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddDiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_diary)
+    }
+}
