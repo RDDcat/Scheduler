730feac6a215b54adb16df334412c0f920ab013f
RDDcat <yukmaro@gmail.com>
2022-03-12 10:19:16 +0900

    [ADD] 안드로이드 액티비티 추가

---
+++ Scheduler/app/src/main/java/com/example/scheduler/AddCalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_calendar)
+    }
+}
---
+++ Scheduler/app/src/main/java/com/example/scheduler/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
