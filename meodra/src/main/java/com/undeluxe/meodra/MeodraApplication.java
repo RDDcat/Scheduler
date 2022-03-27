1e2a16ffa2cc82f39fb55974b1deca46f93c575d
RDDcat <yukmaro@gmail.com>
2022-03-27 10:12:26 +0900

    [ADD] 서버 프로젝트 생성

---
+++ meodra/src/main/java/com/undeluxe/meodra/MeodraApplication.java
@@ -0,0 +1,13 @@
+package com.undeluxe.meodra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeodraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeodraApplication.class, args);
+	}
+
+}
---
+++ meodra/src/test/java/com/undeluxe/meodra/MeodraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undeluxe.meodra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeodraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
