2afab0b1e775f5c5ea45c8e4126f00f906f5a281
nulzi <kimty131@naver.com>
2022-04-02 11:48:20 +0900

    [DELETE] 기존 다이어로그를 사용할 때 만들었던 액티비티, xml [UPDATE] main액티비티에서 메뉴창 visibility로 만들기

--- Scheduler/app/src/main/java/com/example/scheduler/MainActivity.kt
+++ Scheduler/app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.scheduler
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     lateinit var button : Button
@@ -15,52 +18,100 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.MenuButton)
+        var linear = findViewById<LinearLayout>(R.id.linear)
         button.setOnClickListener {
-            val popupView = LayoutInflater.from(this).inflate(R.layout.menu_popup, null)
-            val mBuilder = AlertDialog.Builder(this).setView(popupView)
-
-            val mAlertDialog = mBuilder.show()
-            val mtocButton = popupView.findViewById<Button>(R.id.CalendarButton)
+            if(linear.isVisible){
+                linear.setVisibility(View.GONE)
+            }
+            else {
+                linear.setVisibility(View.VISIBLE)
+            }
+            val mtocButton = findViewById<Button>(R.id.CalendarButton)
             mtocButton.setOnClickListener {
                 val mtocIntent = Intent(this, CalendarActivity::class.java)
                 startActivity(mtocIntent)
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
-            val mtodButton = popupView.findViewById<Button>(R.id.DiaryListButton)
+            val mtodButton = findViewById<Button>(R.id.DiaryListButton)
             mtodButton.setOnClickListener {
                 val mtodlIntent = Intent(this, DiaryListActivity::class.java)
                 startActivity(mtodlIntent)
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
-            val mtoacButton = popupView.findViewById<Button>(R.id.AddCalendarButton)
+            val mtoacButton = findViewById<Button>(R.id.AddCalendarButton)
             mtoacButton.setOnClickListener {
                 val mtoacIntent = Intent(this, AddCalendarActivity::class.java)
                 startActivity(mtoacIntent)
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
-            val mtosButton = popupView.findViewById<Button>(R.id.SettingButton)
+            val mtosButton = findViewById<Button>(R.id.SettingButton)
             mtosButton.setOnClickListener {
                 val mtosIntent = Intent(this, SettingActivity::class.java)
                 startActivity(mtosIntent)
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
-            val mtotdButton = popupView.findViewById<Button>(R.id.ToDoListButton)
+            val mtotdButton = findViewById<Button>(R.id.ToDoListButton)
             mtotdButton.setOnClickListener {
                 val mtotdlIntent = Intent(this, ToDoListActivity::class.java)
                 startActivity(mtotdlIntent)
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
-            val mtoedButton = popupView.findViewById<Button>(R.id.EmotionDiaryButton)
+            val mtoedButton = findViewById<Button>(R.id.EmotionDiaryButton)
             mtoedButton.setOnClickListener {
                 val mtoedIntent = Intent(this, EmotionDiaryActivity::class.java)
                 startActivity(mtoedIntent)
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
-            val shareButton = popupView.findViewById<Button>(R.id.ShareButton)
+            val shareButton = findViewById<Button>(R.id.ShareButton)
             shareButton.setOnClickListener {
                 Toast.makeText(applicationContext, "공유하기", Toast.LENGTH_SHORT).show()
-                mAlertDialog.dismiss()
+                linear.setVisibility(View.GONE)
             }
+//            val popupView = LayoutInflater.from(this).inflate(R.layout.menu_popup, null)
+//            val mBuilder = AlertDialog.Builder(this).setView(popupView)
+//
+//            val mAlertDialog = mBuilder.show()
+//            val mtocButton = popupView.findViewById<Button>(R.id.CalendarButton)
+//            mtocButton.setOnClickListener {
+//                val mtocIntent = Intent(this, CalendarActivity::class.java)
+//                startActivity(mtocIntent)
+//                mAlertDialog.dismiss()
+//            }
+//            val mtodButton = popupView.findViewById<Button>(R.id.DiaryListButton)
+//            mtodButton.setOnClickListener {
+//                val mtodlIntent = Intent(this, DiaryListActivity::class.java)
+//                startActivity(mtodlIntent)
+//                mAlertDialog.dismiss()
+//            }
+//            val mtoacButton = popupView.findViewById<Button>(R.id.AddCalendarButton)
+//            mtoacButton.setOnClickListener {
+//                val mtoacIntent = Intent(this, AddCalendarActivity::class.java)
+//                startActivity(mtoacIntent)
+//                mAlertDialog.dismiss()
+//            }
+//            val mtosButton = popupView.findViewById<Button>(R.id.SettingButton)
+//            mtosButton.setOnClickListener {
+//                val mtosIntent = Intent(this, SettingActivity::class.java)
+//                startActivity(mtosIntent)
+//                mAlertDialog.dismiss()
+//            }
+//            val mtotdButton = popupView.findViewById<Button>(R.id.ToDoListButton)
+//            mtotdButton.setOnClickListener {
+//                val mtotdlIntent = Intent(this, ToDoListActivity::class.java)
+//                startActivity(mtotdlIntent)
+//                mAlertDialog.dismiss()
+//            }
+//            val mtoedButton = popupView.findViewById<Button>(R.id.EmotionDiaryButton)
+//            mtoedButton.setOnClickListener {
+//                val mtoedIntent = Intent(this, EmotionDiaryActivity::class.java)
+//                startActivity(mtoedIntent)
+//                mAlertDialog.dismiss()
+//            }
+//            val shareButton = popupView.findViewById<Button>(R.id.ShareButton)
+//            shareButton.setOnClickListener {
+//                Toast.makeText(applicationContext, "공유하기", Toast.LENGTH_SHORT).show()
+//                mAlertDialog.dismiss()
+//            }
         }
     }
 }
