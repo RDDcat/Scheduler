e98152f7794f2870954ee8a5debbae652eb34e1b
nulzi <kimty131@naver.com>
2022-04-16 10:07:44 +0900

    [UPDATE] 메뉴 버튼을 레이아웃 안으로 변경

--- Scheduler/app/src/main/java/com/example/scheduler/MainActivity.kt
+++ Scheduler/app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -17,54 +17,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.MenuButton)
-        val linear = findViewById<LinearLayout>(R.id.linear)
+        val hideMenu = findViewById<LinearLayout>(R.id.layoutHideMenu)
         button.setOnClickListener {
-            if(linear.isVisible){
-                linear.visibility = View.GONE
+            if(hideMenu.isVisible){
+                hideMenu.visibility = View.GONE
             }
             else {
-                linear.visibility = View.VISIBLE
+                hideMenu.visibility = View.VISIBLE
             }
             val mtocButton = findViewById<Button>(R.id.CalendarButton)
             mtocButton.setOnClickListener {
                 val mtocIntent = Intent(this, CalendarActivity::class.java)
                 startActivity(mtocIntent)
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
             val mtodButton = findViewById<Button>(R.id.DiaryListButton)
             mtodButton.setOnClickListener {
                 val mtodlIntent = Intent(this, DiaryListActivity::class.java)
                 startActivity(mtodlIntent)
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
             val mtoacButton = findViewById<Button>(R.id.AddCalendarButton)
             mtoacButton.setOnClickListener {
                 val mtoacIntent = Intent(this, AddCalendarActivity::class.java)
                 startActivity(mtoacIntent)
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
             val mtosButton = findViewById<Button>(R.id.SettingButton)
             mtosButton.setOnClickListener {
                 val mtosIntent = Intent(this, SettingActivity::class.java)
                 startActivity(mtosIntent)
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
             val mtotdButton = findViewById<Button>(R.id.ToDoListButton)
             mtotdButton.setOnClickListener {
                 val mtotdlIntent = Intent(this, ToDoListActivity::class.java)
                 startActivity(mtotdlIntent)
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
             val mtoedButton = findViewById<Button>(R.id.EmotionDiaryButton)
             mtoedButton.setOnClickListener {
                 val mtoedIntent = Intent(this, EmotionDiaryActivity::class.java)
                 startActivity(mtoedIntent)
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
             val shareButton = findViewById<Button>(R.id.ShareButton)
             shareButton.setOnClickListener {
                 Toast.makeText(applicationContext, "공유하기", Toast.LENGTH_SHORT).show()
-                linear.visibility = View.GONE
+                hideMenu.visibility = View.GONE
             }
         }
 
