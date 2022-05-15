d8cbbfd42e0655c48e6fcf4412f915a1067e9d7e
nulzi <kimty131@naver.com>
2022-05-15 11:25:38 +0900

    [UPDATE] 메인 액티비티에 스크롤뷰 적용

--- Scheduler/app/src/main/java/com/example/scheduler/MainActivity.kt
+++ Scheduler/app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.example.scheduler
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.*
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var button : Button
+    lateinit var tv: TextView
+    lateinit var sv: HorizontalScrollView
+    lateinit var layout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setFullScreen()
+        setTvDate()
 
         button = findViewById(R.id.MenuButton)
         val hideMenu = findViewById<LinearLayout>(R.id.layoutHideMenu)
@@ -80,12 +83,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        tv = findViewById(R.id.tvDate5)
+        tv.setBackgroundColor(Color.parseColor("#ff00ff"))
+        sv = findViewById(R.id.scrollView)
+        sv.isHorizontalScrollBarEnabled = false
+
         val postit = findViewById<ImageView>(R.id.img_post)
         postit.setOnClickListener {
             val mtotdlintent = Intent(applicationContext,ToDoListActivity::class.java)
             startActivity(mtotdlintent)
             hideMenu.visibility = View.GONE
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        layout = findViewById(R.id.svView4)
+        sv = findViewById(R.id.scrollView)
+        layout.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                sv.smoothScrollTo(layout.left,0)
+                layout.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        })
     }
     private fun setFullScreen(){
         supportActionBar?.hide()
@@ -104,6 +124,20 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+    }
+    private fun setTvDate(){
+        var dateary = arrayOf("일","월","화","수","목","금","토")
+        var tvIdAry = arrayOf(R.id.tvDate0,R.id.tvDate1,R.id.tvDate2,R.id.tvDate3,R.id.tvDate4,R.id.tvDate5,R.id.tvDate6,R.id.tvDate7,R.id.tvDate8,
+            R.id.tvDate9,R.id.tvDate10,R.id.tvDate11,R.id.tvDate12,R.id.tvDate13,R.id.tvDate14,R.id.tvDate15,R.id.tvDate16)
+        var tvAry = arrayOfNulls<TextView>(17)
+        for(i in tvIdAry.indices){
+            tvAry[i] = findViewById(tvIdAry[i])
+        }
+        for(i in tvAry.indices) {
+            var calendar = Calendar.getInstance()
+            calendar.add(Calendar.DAY_OF_MONTH,(i-5))
+            tvAry[i]!!.setText("${dateary[calendar.get(Calendar.DAY_OF_WEEK) - 1]} (${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.DAY_OF_MONTH)})")
         }
     }
 }
